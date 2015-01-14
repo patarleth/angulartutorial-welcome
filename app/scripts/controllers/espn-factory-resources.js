b6313849325f96fd5f26e245b8b05e0fbadd4932
Pat Arleth <arleth@about.me>
2015-01-14 17:40:26 -0500

    works now

--- app/scripts/controllers/espn-factory-resources.js
+++ app/scripts/controllers/espn-factory-resources.js
@@ -1,10 +1,5 @@
 'use strict';
 
 angular.module('ngTutorialApp')
-  .controller('EspnFactoryResourcesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ .controller('EspnFactoryResourcesCtrl', function () {
+});
--- test/spec/controllers/espn-factory-resoruces.js
+++ test/spec/controllers/espn-factory-resoruces.js
@@ -2,23 +2,21 @@
 
 describe('Controller: EspnFactoryResorucesCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('ngTutorialApp'));
-
-  var EspnFactoryResorucesCtrl,
+    // load the controller's module
+    beforeEach(module('ngTutorialApp'));
+    
+    var EspnFactoryResorucesCtrl,
     scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-/*
-    EspnFactoryResorucesCtrl = $controller('EspnFactoryResorucesCtrl', {
-      $scope: scope
-    });
-*/
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(!!!EspnFactoryResorucesCtrl).toBe(true);
+    
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        EspnFactoryResorucesCtrl = $controller('EspnFactoryResourcesCtrl', {
+            $scope: scope
+          });
+      }));
+    
+    it('should attach a list of awesomeThings to the scope', function () {
+        expect(!!EspnFactoryResorucesCtrl).toBe(true);
+      });
   });
-});
